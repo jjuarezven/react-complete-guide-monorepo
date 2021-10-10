caed2d0070ce9430b5da893a6ce5b1800b0a7f12
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-10-10 15:06:09 -0500

    Using useReducer

--- projects/usestate-vs-usereducer/src/Login.js
+++ projects/usestate-vs-usereducer/src/Login.js
@@ -1,27 +1,81 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { loginUser } from "./utils";
 
+const loginReducer = (state, action) => {
+  switch (action.type) {
+    case "field":
+      return {
+        ...state,
+        [action.field]: action.value
+      };
+    case "login":
+      return {
+        ...state,
+        isLoading: true,
+        error: ""
+      };
+    case "success":
+      return {
+        ...state,
+        isLoggedIn: true
+      };
+    case "error":
+      return {
+        ...state,
+        error: "Incorrect username or password!",
+        isLoading: false,
+        username: "",
+        password: ""
+      };
+    case "logout":
+      return {
+        ...state,
+        isLoggedIn: false,
+        isLoading: false,
+        username: "",
+        password: ""
+      };
+    default:
+      break;
+  }
+  return state;
+};
+
+const initialState = {
+  username: "",
+  password: "",
+  isLoading: false,
+  error: "",
+  isLoggedIn: ""
+};
+
 export default function Login() {
-  const [username, setUsername] = useState("");
+  /* const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, showLoader] = useState(false);
   const [error, setError] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); */
+
+  const [state, dispatch] = useReducer(loginReducer, initialState);
+  const { username, password, isLoading, error, isLoggedIn } = state;
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    setError("");
-    showLoader(true);
+    /* setError("");
+    showLoader(true); */
+    dispatch({ type: "login" });
 
     try {
       await loginUser({ username, password });
-      setIsLoggedIn(true);
+      //setIsLoggedIn(true);
+      dispatch({ type: "success" });
     } catch (error) {
-      setError("Incorrect username or password!");
+      /* setError("Incorrect username or password!");
       showLoader(false);
       setUsername("");
-      setPassword("");
+      setPassword(""); */
+      dispatch({ type: "error" });
     }
   };
 
@@ -31,7 +85,9 @@ export default function Login() {
         {isLoggedIn ? (
           <>
             <h1>Welcome {username}!</h1>
-            <button onClick={() => setIsLoggedIn(false)}>Log Out</button>
+            <button onClick={() => dispatch({ type: "logout" })}>
+              Log Out
+            </button>
           </>
         ) : (
           <form className="form" onSubmit={onSubmit}>
@@ -41,14 +97,28 @@ export default function Login() {
               type="text"
               placeholder="username"
               value={username}
-              onChange={(e) => setUsername(e.currentTarget.value)}
+              onChange={(e) =>
+                dispatch({
+                  type: "field",
+                  field: "username",
+                  value: e.currentTarget.value
+                })
+              }
+              /* onChange={(e) => setUsername(e.currentTarget.value)} */
             />
             <input
               type="password"
               placeholder="password"
               autoComplete="new-password"
               value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
+              onChange={(e) =>
+                dispatch({
+                  type: "field",
+                  field: "password",
+                  value: e.currentTarget.value
+                })
+              }
+              /* onChange={(e) => setPassword(e.currentTarget.value)} */
             />
             <button className="submit" type="submit" disabled={isLoading}>
               {isLoading ? "Logging in..." : "Log In"}
