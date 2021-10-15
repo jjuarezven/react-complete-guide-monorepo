027460d50875d4723b9b5f3dab50bcf1f7c7c711
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-10-15 17:15:07 -0500

    Adding modal with Portals for displaying cart

--- projects/practice-project-module-11/src/App.js
+++ projects/practice-project-module-11/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Cart from "./Components/Cart/Cart.js";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals.js";
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
