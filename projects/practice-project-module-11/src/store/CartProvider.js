a016c732d46425f6b08fbc7e8ebbabb24acbc59a
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-10-18 18:14:12 -0500

    Adding logic to update items if exists

--- projects/practice-project-module-11/src/store/CartProvider.js
+++ projects/practice-project-module-11/src/store/CartProvider.js
@@ -9,9 +9,28 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     console.log("step 3: update state on CartProvider");
-    const updatedItems = state.items.concat(action.item);
+
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    let updatedItems;
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
