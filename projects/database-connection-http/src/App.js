ce08e05c117f13a3c5a652023a57e6ea290256cb
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-10-21 16:18:05 -0500

    Using async await

--- projects/database-connection-http/src/App.js
+++ projects/database-connection-http/src/App.js
@@ -6,8 +6,20 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films")
+  const fetchMoviesHandler = async () => {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      };
+    });
+    setMovies(transformedMovies);
+    /* fetch("https://swapi.dev/api/films")
       .then((response) => {
         return response.json();
       })
@@ -21,7 +33,7 @@ function App() {
           };
         });
         setMovies(transformedMovies);
-      });
+      }); */
   };
 
   return (
