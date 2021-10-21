48cf71cfd3fc980012be4c55dba8a2f495fc58e1
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-10-20 20:00:25 -0500

    Preventing unnecesary re evaluations using React.memo

--- projects/react-behind-scenes-optimization/src/components/Demo/DemoOutput.js
+++ projects/react-behind-scenes-optimization/src/components/Demo/DemoOutput.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React from "react";
 
 const DemoOutput = (props) => {
-  console.log('DemoOutput RUNNING');
-  return <p>{props.show ? 'This is new!' : ''}</p>;
+  console.log("DemoOutput RUNNING");
+  return <p>{props.show ? "This is new!" : ""}</p>;
 };
 
-export default DemoOutput;
+export default React.memo(DemoOutput);
+
+// in this case, React.memo works because props.show is primitive value and comparison between primitive values returns true
--- projects/react-behind-scenes-optimization/src/components/UI/Button/Button.js
+++ projects/react-behind-scenes-optimization/src/components/UI/Button/Button.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Button.module.css';
+import classes from "./Button.module.css";
 
 const Button = (props) => {
+  console.log("Button RUNNING");
   return (
     <button
-      type={props.type || 'button'}
+      type={props.type || "button"}
       className={`${classes.button} ${props.className}`}
       onClick={props.onClick}
       disabled={props.disabled}
@@ -15,4 +16,6 @@ const Button = (props) => {
   );
 };
 
-export default Button;
+export default React.memo(Button);
+
+// in this case, React.memo does not work because props.onClick is reference value and comparison between reference values returns false
