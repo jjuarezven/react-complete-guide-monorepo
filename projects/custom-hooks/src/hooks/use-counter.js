eaf2911effd1b5a270241606b48f498374d2a94e
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-10-24 16:28:02 -0500

    Introducing use of custom hook

---
+++ projects/custom-hooks/src/hooks/use-counter.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+// we could use the entire function ( (prevCounter) => prevCounter + 1 ) instead of the boolean parameter
+const useCounter = (forwards = true) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwards]);
+
+  return counter;
+};
+
+export default useCounter;
