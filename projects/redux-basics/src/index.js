4a640e00759d59be9bfc7110335bbed239281047
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-11-01 16:24:18 -0500

    Providing the store

--- projects/redux-basics/src/index.js
+++ projects/redux-basics/src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
-import './index.css';
-import App from './App';
+// 3 se debe registrar el store para que sea accesible a los componentes que se desea hagan uso de el, para ello se debe
+// hacer un wrap del Provider sobre componente root App y pasarle como prop el store que definimos previamente
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
