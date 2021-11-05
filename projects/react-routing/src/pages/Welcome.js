50421a49f08b02d97d3961b2f93da37bb9f18c94
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-11-05 17:07:22 -0500

    Using nested routes

--- projects/react-routing/src/pages/Welcome.js
+++ projects/react-routing/src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>The welcome page!</h1>;
+  return (
+    <section>
+      <h1>The welcome page!</h1>
+      <Route path="/welcome/new-user">
+        <h2>Welcome new user</h2>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
