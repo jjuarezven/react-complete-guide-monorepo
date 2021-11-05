df446676517b241a841424e36cfffc7e1a4264cf
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-11-05 16:59:03 -0500

    Dynamic routes with params

--- projects/react-routing/src/pages/Products.js
+++ projects/react-routing/src/pages/Products.js
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
-  return <h1>The products page!</h1>;
+  return (
+    <section>
+      <h1>The Products page</h1>
+      <ul>
+        <li>
+          <Link to="/products/1">A book</Link>
+        </li>
+        <li>
+          <Link to="/products/2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/3">Online course</Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
