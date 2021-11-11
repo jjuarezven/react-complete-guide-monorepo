cb75ddf5a6ca3ba320716280243b6689b8c47e00
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-11-11 17:29:01 -0500

    Adding user login

--- projects/authentication/src/components/Auth/AuthForm.js
+++ projects/authentication/src/components/Auth/AuthForm.js
@@ -22,36 +22,77 @@ const AuthForm = () => {
     // optional: Add validation
 
     setIsLoading(true);
+    let url;
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBrG8ICNXb0JsTc4cleePxj2Kz5vlXAaGk";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBrG8ICNXb0JsTc4cleePxj2Kz5vlXAaGk",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true
-          }),
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBrG8ICNXb0JsTc4cleePxj2Kz5vlXAaGk";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
-          // ...
+          return res.json();
         } else {
           return res.json().then((data) => {
             let errorMessage = "Authentication failed!";
             if (data && data.error && data.error.message) {
               errorMessage = data.error.message;
             }
-            alert(errorMessage);
+
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
+    /*
+      cleaner code
+      const sendRequest = async (url, email, password) => {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+ 
+    setIsLoading(false);
+ 
+    try {
+      if (!response.ok) {
+        const data = await response.json();
+ 
+        throw new Error(data.error.message);
+      }
+      const data = await response.json();
+      // console.log(data)
+    } 
+    catch (err) {
+      alert(err);
     }
+  };
+      */
   };
 
   return (
