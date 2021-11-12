e70aab02f0d644938d14f9bec6561f46db68433c
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-11-11 19:50:41 -0500

    Sending token to access protected resources

--- projects/authentication/src/components/Profile/ProfileForm.js
+++ projects/authentication/src/components/Profile/ProfileForm.js
@@ -1,17 +1,50 @@
-import classes from './ProfileForm.module.css';
+import classes from "./ProfileForm.module.css";
+import { useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBrG8ICNXb0JsTc4cleePxj2Kz5vlXAaGk",
+      {
+        method: "POST",
+        // note that this API requires the token be part of an object, others API could manage it differently (Authorization header, etc)
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: false
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    ).then((res) => {
+      // assumption: Always succeeds!
+    });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          type="password"
+          id="new-password"
+          ref={newPasswordInputRef}
+          minLength="7"
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
