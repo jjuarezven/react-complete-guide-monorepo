15efc3a57fe634a23742648c75b5aea08d8282ff
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-11-16 16:34:33 -0500

    Using _app.js

--- projects/nextjs-sample/pages/_app.js
+++ projects/nextjs-sample/pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/layout/Layout";
 
+// root component where we add any general component instead of add on every particular component
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
