7582d7c78f562cf910a827d1ab6852e3908f6ba6
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-11-16 21:22:37 -0500

    Working with params for ssg data fetching (getStaticProps) and using getStaticPaths

--- projects/nextjs-sample/pages/[meetupId]/index.js
+++ projects/nextjs-sample/pages/[meetupId]/index.js
@@ -1,14 +1,49 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      title="First Meetup"
-      address="Some Street 5, Some City"
-      description="This is a first meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
+
+export const getStaticProps = async (context) => {
+  // fetch data for single meetup
+  const meetupId = context.params.meetupId;
+  // only see on the VS code terminal, not browser console
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup"
+      }
+    }
+  };
+};
+
+export const getStaticPaths = async () => {
+  // must declare all available ids to prerender
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: { meetupId: "m1" }
+      },
+      {
+        params: { meetupId: "m2" }
+      }
+    ]
+  };
+};
 
 export default MeetupDetails;
