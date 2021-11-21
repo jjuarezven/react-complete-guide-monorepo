750da52f080a3bf8b15c51d18e747e6697e0d6bb
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-11-20 20:50:25 -0500

    Using custom hook to replace Redux

---
+++ projects/replace-redux-with-context/src/hooks-store/store.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+// thse variables are outside of the component so they will keep its value no matter if the component re render
+let globalState = {};
+let listeners = []; // We need to know which components are interested in state changes
+let actions = {};
+
+export const useStore = (shouldListen = true) => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier, payload) => {
+    const newState = actions[actionIdentifier](globalState, payload);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    if (shouldListen) {
+      listeners.push(setState);
+    }
+
+    return () => {
+      if (shouldListen) {
+        listeners = listeners.filter((li) => li !== setState);
+      }
+    };
+  }, [setState, shouldListen]);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+};
