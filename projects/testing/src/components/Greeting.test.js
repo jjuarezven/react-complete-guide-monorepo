37fb52390c462a8d871b77e4e595e52b2b6d61d3
josejuarez <79205538+josejmill5@users.noreply.github.com>
2021-11-20 21:57:01 -0500

    Testing user interaction and state

--- projects/testing/src/components/Greeting.test.js
+++ projects/testing/src/components/Greeting.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting.js";
+import userEvent from "@testing-library/user-event";
 
 describe("Greeting component", () => {
   test("Renders Hello World as a text", () => {
@@ -7,5 +8,33 @@ describe("Greeting component", () => {
 
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("Renders It's good to see you! if the button was not clicked", () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText("It's good to see you!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("Renders Changed! if the button was not clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("Does not render It's good to see you! if the button was clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // queryByText returns null if text is not found
+    const outputElement = screen.queryByText("It's good to see you!");
+    expect(outputElement).toBeNull();
   });
 });
